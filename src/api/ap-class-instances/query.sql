b0bce917cf4dc26296ddc03ad1cb862cde3cc426
Jonathan <coleji@bc.edu>
2017-04-09 23:48:35 -0400

    add location_string to ap_class_instances

--- src/api/ap-class-instances/query.sql
+++ src/api/ap-class-instances/query.sql
@@ -1,4 +1,4 @@
-select i.instance_id, t.type_name, to_char(fs.session_datetime, 'MM/DD/YYYY') as start_date, to_char(fs.session_datetime, 'HH:MIPM') as start_time,
+select i.instance_id, t.type_name, to_char(fs.session_datetime, 'MM/DD/YYYY') as start_date, to_char(fs.session_datetime, 'HH:MIPM') as start_time, i.location_string,
 (select count(*) from ap_class_signups where instance_id = i.instance_id and signup_type = 'E') as enrollees
 from ap_class_types t, ap_class_formats f, ap_class_instances i, ap_class_bookends bk, ap_class_sessions fs
 where i.instance_id = bk.instance_id and bk.first_session = fs.session_id
