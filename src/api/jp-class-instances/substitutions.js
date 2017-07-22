4ec0bc5ff2cbc858fb8b498ee9644c2c61b5fa66
Jonathan <coleji@bc.edu>
2017-07-22 11:47:57 -0400

    jp-class-instances: need to recompute default startdate on each new request

--- src/api/jp-class-instances/substitutions.js
+++ src/api/jp-class-instances/substitutions.js
@@ -2,18 +2,18 @@ import moment from 'moment';
 
 const QUERY_PARAMS = {
 	"startDate" : {
-		default: moment().format("MM/DD/YYYY"),
+		default: () => moment().format("MM/DD/YYYY"),
 		isValid : value => {
 			return /^[0-9]{2}\/[0-9]{2}\/[0-9]{4}$/.test(value);
 		}
 	}
 };
 
-export default (sql, urlQuery) => new Promise((resolve, reject) => {
+export default (sql, urlQuery) => new Promise((resolve) => {
 	var startDate = (function() {
 		var spec = QUERY_PARAMS.startDate
 		if (!urlQuery.startDate || !spec.isValid(urlQuery.startDate)) {
-			return spec.default
+			return spec.default()
 		} else {
 			return urlQuery.startDate
 		}
