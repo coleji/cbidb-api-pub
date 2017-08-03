39440b3e7eb77f08c535aa9b1263ea981bbbcad8
Jonathan <coleji@bc.edu>
2017-08-03 07:21:27 -0400

    jp-teams: include full name for scala switchover compatibility

--- src/api/jp-teams/query.sql
+++ src/api/jp-teams/query.sql
@@ -1,7 +1,7 @@
 select team_name, points from (
-  select 'Blue' as team_name, sum(points) as points from jp_team_event_points where team_id = 1
+  select 'Blue Team' as team_name, sum(points) as points from jp_team_event_points where team_id = 1
   union all
-  select 'Orange' as team_name, sum(points) as points from jp_team_event_points where team_id = 2
+  select 'Orange Team' as team_name, sum(points) as points from jp_team_event_points where team_id = 2
   union all
-  select 'Silver' as team_name, sum(points) as points from jp_team_event_points where team_id = 3
+  select 'Silver Team' as team_name, sum(points) as points from jp_team_event_points where team_id = 3
 )
