6f580d3a2a8f01509a3c849e4aa101d1b72207a7
Jonathan <coleji@bc.edu>
2018-06-10 10:37:21 -0400

    update teams: filter to current year

--- src/api/jp-teams/query.sql
+++ src/api/jp-teams/query.sql
@@ -1,7 +1,7 @@
 select team_name, points from (
-  select 'Blue Team' as team_name, sum(points) as points from jp_team_event_points where team_id = 1
+  select 'Blue Team' as team_name, nvl(sum(points),0) as points from jp_team_event_points ep, jp_team_events e where ep.event_id = e.event_id and to_char(e.awarded_date,'YYYY') = to_char(sysdate,'YYYY') and team_id = 1
   union all
-  select 'Orange Team' as team_name, sum(points) as points from jp_team_event_points where team_id = 2
+  select 'Orange Team' as team_name, nvl(sum(points),0) as points from jp_team_event_points ep, jp_team_events e where ep.event_id = e.event_id and to_char(e.awarded_date,'YYYY') = to_char(sysdate,'YYYY') and team_id = 2
   union all
-  select 'Silver Team' as team_name, sum(points) as points from jp_team_event_points where team_id = 3
+  select 'Silver Team' as team_name, nvl(sum(points),0) as points from jp_team_event_points ep, jp_team_events e where ep.event_id = e.event_id and to_char(e.awarded_date,'YYYY') = to_char(sysdate,'YYYY') and team_id = 3
 )
